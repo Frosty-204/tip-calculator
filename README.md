6082307056eab27dca17bf0e753e57b535940641
Frosty-204 <adinar204@gmail.com>
2022-10-15 22:37:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+A simple responsive mobile friendly tip calculator with a reset button.
+
+### Screenshot
+
+![Screenshot of the complete version.](../tip-calculator-app-main/images/screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Javascript
+-
+
+### What I learned
+
+I really gone through much here 😅, looping through the tip buttons and toggeling the states was kinda weird for me, not to mention how i tried to give up when i didn't know how to seperate the custom tip from the tip buttons. So it was actually quite a decent challange for a beginner like me.
+
+### Useful resources
+
+- [Icon inside fields](https://stackoverflow.com/questions/917610/put-icon-inside-input-element-in-a-form) - This helped me on how to put icons inside the input fields.
---
+++ js/main.js
@@ -0,0 +1,118 @@
+"use strict";
+
+// DESC the app
+
+//////////////////////////////////
+// VU Declaraings
+//////////////////////////////////
+const billInput = document.querySelector("#billInput");
+const tipOptionBtnContianer = document.querySelectorAll(
+  "#tipOptionContainer button"
+);
+const splitInput = document.querySelector("#splitInput");
+const tips = document.querySelectorAll(".tip-option");
+const tipCustom = document.querySelector("#tipInput");
+const totalTip = document.querySelector("#tipFinal");
+const totalBill = document.querySelector("#totalFinal");
+const resetBtn = document.querySelector("#resetEl");
+const errorMsg = document.querySelector(".error-msg");
+
+let billValue;
+let tipValue = 0.15;
+let splitValue = 1;
+//////////////////////////////////
+// VU Logic
+//////////////////////////////////
+
+// show btn
+
+// Handle Tip
+const handleTip = function (e) {
+  // DESC Handle the styles and logic
+  tips.forEach((btn) => {
+    // Reset states
+    btn.classList.remove("active");
+
+    // Add active state
+    if (e.target.innerHTML == btn.innerHTML) {
+      btn.classList.add("active");
+      tipValue = parseFloat(btn.innerHTML) / 100;
+    }
+  });
+
+  tipCustom.value = "";
+  // Guard if to prevent NaN occurance
+  if (billValue) calcTipAmount();
+};
+
+const calcTipAmount = function () {
+  if (splitValue >= 1) {
+    let tipAmount = (billValue * tipValue) / splitValue;
+    let total = (billValue * (tipValue + 1)) / splitValue;
+
+    // Display totals
+    totalTip.textContent = `$ ${tipAmount.toFixed(2)}`;
+    totalBill.textContent = `$ ${total.toFixed(2)}`;
+  }
+
+  // Make button active
+  resetBtn.classList.add("btn-active");
+  resetBtn.classList.remove("btn-off");
+};
+
+// Get Custom tip tricky 😅
+
+const getCustomValue = function () {
+  tipValue = parseFloat(tipCustom.value) / 100;
+
+  tips.forEach((btn) => btn.classList.remove("active"));
+
+  // Guard
+  if (tipCustom.value !== "") calcTipAmount();
+};
+
+// VU Events
+
+// BILL take numbers from the input field
+
+billInput.addEventListener("change", function () {
+  billValue = parseFloat(billInput.value);
+  calcTipAmount();
+});
+
+// SPLIT
+splitInput.addEventListener("change", function () {
+  // Render error message then disappear after two seconds
+  if (splitInput.value == 0) {
+    splitInput.classList.add("error");
+    errorMsg.style.display = "block";
+
+    setTimeout(() => {
+      splitInput.classList.remove("error");
+      errorMsg.style.display = "none";
+    }, 2000);
+  }
+  splitValue = +splitInput.value;
+  calcTipAmount();
+});
+
+// Reset
+
+const reset = function () {
+  // chain of setting all of them to 0
+  billInput.value = splitInput.value = tipValue = billValue = splitValue = 0;
+  totalBill.textContent = totalTip.textContent = `$ 0.00`;
+
+  // Make button off again
+  resetBtn.classList.remove("btn-active");
+  resetBtn.classList.add("btn-off");
+  tips.forEach((btn) => btn.classList.remove("active"));
+};
+
+// TIP Functions above
+tips.forEach((tip) => {
+  tip.addEventListener("click", handleTip);
+});
+
+tipCustom.addEventListener("change", getCustomValue);
+resetBtn.addEventListener("click", reset);
